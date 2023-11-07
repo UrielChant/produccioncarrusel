ddaeb7317d398787f9f337395bf949028f569f34
UrielChant <147094967+UrielChant@users.noreply.github.com>
2023-11-07 12:29:30 -0600

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-url_endpoint='https://api-carousel-sandbox.nueve09.io/v1/';//'https://api-carousel-prod.nueve09.io/v1/'; 
+url_endpoint='https://api-carousel-prod.nueve09.io/v1/';//'https://api-carousel-prod.nueve09.io/v1/'; 
 var temporal="";
 var identidad='';
 //------------------Inicio de sesion url+login------------------------
