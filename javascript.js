a485bf956f6fb2da2d236175ff981ebaf6e3f864
UrielChant <147094967+UrielChant@users.noreply.github.com>
2023-11-08 16:37:09 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,10 +1,11 @@
 url_endpoint='https://api-carousel-prod.nueve09.io/v1/';//'https://api-carousel-prod.nueve09.io/v1/'; 
 var temporal="";
-var identidad='';
+var identidad="";
 //------------------Inicio de sesion url+login------------------------
 async function iniciarSesion() {
     const email= document.getElementById('email').value;
     const password = document.getElementById('password').value;
+    
 
     const data = {
         email,
@@ -22,11 +23,14 @@ async function iniciarSesion() {
         if (response.ok) {
             const primero  = await response.json();
             token=primero.data.token;
+            console.log(token);
             user=primero.data.user.email;
             localStorage.setItem('token',token);
             localStorage.setItem('user', user);
             document.getElementById('login').style.display = 'none';
             document.getElementById('anuncios').style.display = 'block';
+            email.value='';
+            password.value='';
             refrescar();
         } else {
             alert('Credenciales incorrectas. Por favor, inténtalo de nuevo.');
@@ -42,7 +46,7 @@ async function refrescar() {
             //console.log(data1.password);
     try {
         const token=localStorage.getItem('token');
-            const response = await fetch(url_endpoint+'ads?page=1&page_size=18', {//  /mobile   <--este faltante de liga me ayuda a entrar a un entorno de prueba/la paginacion debe ser desde aqui para andar subiendo los archivos
+            const response = await fetch(url_endpoint+'ads?page=1&page_size=24', {//  /mobile   <--este faltante de liga me ayuda a entrar a un entorno de prueba/la paginacion debe ser desde aqui para andar subiendo los archivos
             //ads?page=1&page_size=1
             method: 'GET',
             headers: {
@@ -56,14 +60,15 @@ async function refrescar() {
         if (response.ok) {
             const tableBody = document.querySelector('#tablaDatos tbody');
             tableBody.innerHTML = '';
-            var conteo=0;
-            
+                console.log(fechaHoraActual);
                 ads.forEach(item => {
                     const row = tableBody.insertRow();
-                    row.insertCell(0).textContent = item.name;
-                    row.insertCell(1).textContent = item.alias;
+                    var namecorto=item.name;
+                    row.insertCell(0).textContent = namecorto.substring(namecorto.length-15);
+                    var aliascorto=item.alias;
+                    row.insertCell(1).textContent = aliascorto.substring(aliascorto.length-15);
                     row.insertCell(2).textContent = item.position;
-                    row.insertCell(3).textContent = item.call_to_action;
+                    row.insertCell(3).textContent = item.call_to_action.substring(0,15);
                     row.insertCell(4).textContent = item.id;
                     row.insertCell(5).textContent = item.status;
                     row.insertCell(6).textContent = item.user_id;
@@ -80,7 +85,7 @@ async function refrescar() {
 
                 const deleteButton = document.createElement("button");
                 deleteButton.textContent = "Eliminar";
-                deleteButton.addEventListener("click", () => borraranuncio(item.id));//aqui se coloca el identificador  para realizar supresion
+                deleteButton.addEventListener("click", () =>confirmacion(item.id))
                 row.insertCell(10).appendChild(deleteButton);
 
                 const viewButton = document.createElement("button");
@@ -114,26 +119,27 @@ async function veranuncio(itemId) {
         });
     const data = await response.json();
     const ads=data.data.ad;
-    temporal=[ads.name,ads.alias,ads.position,ads.call_to_action];
+    temporal=[ads.name,ads.alias,ads.position,ads.call_to_action,ads.start_date];
+    console.log(temporal);
     if (response.ok) {
         const tableBody = document.querySelector('#tablaDatosads tbody');
         tableBody.innerHTML = ''; 
 
         var row2 = tableBody.insertRow();
         row2.insertCell(0).textContent = ads.name;
-        row2.insertCell(1).textContent = ads.alias;
+        row2.insertCell(1).textContent = ads.alias.substring(0,10);
         row2.insertCell(2).textContent = ads.position;
         row2.insertCell(3).textContent = ads.call_to_action;
         row2.insertCell(4).textContent = ads.id;
         row2.insertCell(5).textContent = ads.status;
         row2.insertCell(6).textContent = ads.user_id;
-        row2.insertCell(7).textContent = ads.start_date.substring(0,10);
-        row2.insertCell(8).textContent = ads.end_date.substring(0,10);
+        row2.insertCell(7).textContent = ads.start_date;
+        row2.insertCell(8).textContent = ads.end_date;
         identidad=ads.id;
         
         const deleteButton = document.createElement("button");
         deleteButton.textContent = "Eliminar";
-        deleteButton.addEventListener("click", () => borraranuncio(ads.id));//aqui se coloca el identificador  para realizar supresion
+        deleteButton.addEventListener("click", () => confirmacion(ads.id));//aqui se coloca el identificador  para realizar supresion
         row2.insertCell(9).appendChild(deleteButton);
         
         var row2 = tableBody.insertRow();
@@ -144,9 +150,15 @@ async function veranuncio(itemId) {
         image.src=ads.image;
         image.style.maxWidth = "1080px"; 
         image.style.maxHeight = "600px"; 
-        image.style.width = "100%";      
+        image.style.width = "auto";      
         image.style.height = "auto";
         celdai.appendChild(image);
+        // var cadena=ads.alias;
+        // console.log(cadena);
+        // var row2 = tableBody.insertRow();
+        // var celdaalias=row2.insertCell();
+        // celdaalias.colSpan=10;
+        // celdaalias.textContent(cadena);
 
     } else {
         console.error('Error al obtener los datos:');
@@ -179,32 +191,60 @@ document.getElementById("enviar").addEventListener("click", async function() {
     formData.append('position', position);
     formData.append('call_to_action', call_to_action);
     formData.append('image', archivo);
-    
+    // name.value='';
+    //     alias.value='';
+    //     call_to_action.value='';
+    //     start_date.value='';
+    //     end_date.value='';
+    //     position.value='';
+        
+    if((end_date>start_date)&&(position<25)&&((name!=='')&&(alias!=='')&&(image!=='')&&(call_to_action!=='')&&(start_date!=='')&&(end_date!=='')&&(position!==''))){
+        try{
+            const token=localStorage.getItem('token');
+           const response= await fetch(url_endpoint+'ads', {
+                method: "POST",
+                headers:{
+                    //'Content-Type': 'application/json',
+                    'Authorization':`Bearer ${token}`
+                },
+                body: formData
+            })
+        
+            if (response.ok) {
+                alert('Subida exitosa');
+                image.value='';
+                document.getElementById("campo1").value='';
+                document.getElementById("campo2").value='';
+                document.getElementById("campo4").value='';
+                document.getElementById("campo5").value='';
+                document.getElementById("campo6").value='';
+                document.getElementById("campo7").value='';
+                document.getElementById('formulariocarrusel').style.display = 'none';
+                document.getElementById('anuncios').style.display = 'block';
+                refrescar();
+              
+            } else {
+                alert('sucedio un problema.');
+            }
+        }
+            catch(error){
+                console.error("Error:", error);
+            };
+        
 
-try{
-    const token=localStorage.getItem('token');
-   const response= await fetch(url_endpoint+'ads', {
-        method: "POST",
-        headers:{
-            //'Content-Type': 'application/json',
-            'Authorization':`Bearer ${token}`
-        },
-        body: formData
-    })
-
-    if (response.ok) {
-        alert('Subida exitosa');
-        document.getElementById('formulariocarrusel').style.display = 'none';
-        document.getElementById('anuncios').style.display = 'block';
-        refrescar();
-      
-    } else {
-        alert('sucedio un problema.');
     }
-}
-    catch(error){
-        console.error("Error:", error);
-    };
+    else if((position<25)&&((name!=='')&&(alias!=='')&&(image!=='')&&(call_to_action!=='')&&(start_date!=='')&&(end_date!=='')&&(position!==''))){
+        alert('No procede los datos de la fecha no son correctos la fecha de termino debe superior a la fecha de inicio')
+    }
+    else if((end_date>start_date)&&((name!=='')&&(alias!=='')&&(image!=='')&&(call_to_action!=='')&&(start_date!=='')&&(end_date!=='')&&(position!==''))){
+        alert('La posicion no debe de rebasar del numero 25')
+    }
+    else if((position<25)&&(end_date>start_date)){
+        alert('Hay un campo vacio no puedes enviar si hay un campo sin llenar')
+    }
+    else{
+        alert('No procede los datos de la fecha no son correctos fecha de termino debe superior a la fecha de inicio, la posicion no debe de rebasar el numero 25 y hay un campo vacio, no puedes enviar si hay un campo sin llenar.')
+    }
 });
 //-----------------------------------------------------------------------
 
@@ -256,40 +296,55 @@ document.getElementById("actualizar").addEventListener("click", async function()
     // formData.append('position', position);
     // formData.append('call_to_action', call_to_action);
 
-
+    if((end_date>start_date)&&(position<25)&&((name!=='')&&(alias!=='')&&(call_to_action!=='')&&(start_date!=='')&&(end_date!=='')&&(position!==''))){
+        try {
+            const token=localStorage.getItem('token');
+            const response = await fetch(url_endpoint+`ads/${identidad}`, {//aqui se debera de poner el id correspondiente
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization':`Bearer ${token}`
+                },
+                body:JSON.stringify ({
+                                name:name,
+                                alias:alias,
+                                start_date:start_date,
+                                end_date:end_date,
+                                position:position,
+                                call_to_action:call_to_action,
+                                status:boolva       
+                })
+            });
     
-    try {
-        const token=localStorage.getItem('token');
-        const response = await fetch(url_endpoint+`ads/${identidad}`, {//aqui se debera de poner el id correspondiente
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization':`Bearer ${token}`
-            },
-            body:JSON.stringify ({
-                            name:name,
-                            alias:alias,
-                            start_date:start_date,
-                            end_date:end_date,
-                            position:position,
-                            call_to_action:call_to_action,
-                            status:boolva       
-            })
-        });
-
-        if (response.ok) {
-            //const {status2}= await response.json();
-           // console.log('Datos actualizados:', status2);
-            alert("tu anuncio ha sido actualizado con exito");
-            document.getElementById('formulariocarrusel2').style.display = 'none';
-            document.getElementById('anuncios').style.display = 'block';
-            refrescar();
-        } else {
-            console.error('Error al actualizar los datos:');
+            if (response.ok) {
+                //const {status2}= await response.json();
+               // console.log('Datos actualizados:', status2);
+                alert("tu anuncio ha sido actualizado con exito");
+                document.getElementById('formulariocarrusel2').style.display = 'none';
+                document.getElementById('anuncios').style.display = 'block';
+                refrescar();
+            } else {
+                console.error('Error al actualizar los datos:');
+            }
+        } catch (error) {
+            console.error('Error de red:', error);
         }
-    } catch (error) {
-        console.error('Error de red:', error);
+        
+
     }
+    else if((position<25)&&((name!=='')&&(alias!=='')&&(call_to_action!=='')&&(start_date!=='')&&(end_date!=='')&&(position!==''))){
+        alert('No procede los datos de la fecha no son correctos la fecha de termino debe superior a la fecha de inicio')
+    }
+    else if((end_date>start_date)&&((name!=='')&&(alias!=='')&&(call_to_action!=='')&&(start_date!=='')&&(end_date!=='')&&(position!==''))){
+        alert('La posicion no debe de rebasar del numero 25')
+    }
+    else if((position<25)&&(end_date>start_date)){
+        alert('Hay un campo vacio no puedes enviar si hay un campo sin llenar')
+    }
+    else{
+        alert('No procede los datos de la fecha no son correctos fecha de termino debe superior a la fecha de inicio, la posicion no debe de rebasar el numero 25 y hay un campo vacio, no puedes enviar si hay un campo sin llenar.')
+    }
+    
 });
 //---------------------------------------------------------------------------------------------------
 
@@ -315,6 +370,7 @@ document.getElementById("modificar").addEventListener("click", function() {
      campo22.value=temporal[1];
      campo77.value=temporal[2];
      campo44.value=temporal[3];
+     campo55.value=temporal[4];
 
     document.getElementById('anuncioid').style.display = 'none';
     document.getElementById('formulariocarrusel2').style.display = 'block';
@@ -331,6 +387,8 @@ document.getElementById("cerrar").addEventListener("click",  function() {
     localStorage.removeItem('user')
     const tableBody = document.querySelector('#tablaDatos tbody');
     tableBody.innerHTML = '';
+    document.getElementById('email').value='';
+    document.getElementById('password').value='';
     document.getElementById('anuncios').style.display = 'none';
     document.getElementById('login').style.display = 'block';
     })
@@ -399,3 +457,31 @@ document.getElementById('login').addEventListener('button', function(e) {
     return emailRegex.test(email);
   }
 //---------------------------------------------------------------------------------------
+function confirmacion(identificacion){
+var confirmacion = confirm('¿Estás seguro de que deseas realizar esta acción?');
+
+            // Verificar si se confirmó la acción
+            if (confirmacion) {
+                borraranuncio(identificacion);
+                alert('Acción realizada');
+                // Código para la acción
+            } else {
+                // Si el usuario cancela, no se realizará ninguna acción
+                alert('Acción cancelada');
+            }
+        };
+const fechaHoraActual = new Date().toISOString().slice(0, 16);
+        
+    document.getElementById("campo5").min = fechaHoraActual;
+    document.getElementById("campo55").min = fechaHoraActual;
+
+    const startDate = document.getElementById("campo5");
+    const endDate = document.getElementById("campo6");
+    const startDate2 = document.getElementById("campo55");
+    const endDate2 = document.getElementById("campo66");
+    startDate.addEventListener('input', function() {
+        endDate.min = startDate.value;
+    });
+    startDate2.addEventListener('input', function() {
+        endDate2.min = startDate2.value;
+    });
